a45c009f8e13cb62d01968f52a76dd2922700660
Jarren San Jose <sanjose.jarren@gmail.com>
2023-10-30 19:01:19 -0700

    multiply how much they make per day by 250

---
+++ components/Math.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useState, useEffect, ChangeEvent } from 'react';
+
+const Math = () => {
+  const [inputValue, setInputValue] = useState<number | ''>('');
+  const [result, setResult] = useState<number>(0);
+
+  useEffect(() => {
+    if (inputValue !== '') {
+      setResult(inputValue * 250);
+    } else {
+      setResult(0);
+    }
+  }, [inputValue]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value === '' ? '' : parseFloat(value));
+  };
+
+  return (
+    <div>
+      <h1 className="text-4xl">how much did you make today?</h1>
+      <input type="number" value={inputValue} onChange={handleInputChange} />  
+      { inputValue !== '' &&
+        <>
+           &nbsp;a day 
+          <h2 className="text-2xl">is ${result} a year </h2>
+          <p>if there are 250 trading days a year </p>
+        </>
+      }
+    </div>
+  );
+};
+
+export default Math;
