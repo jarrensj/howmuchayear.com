c327d9a7d661c49a952f63e02ce47b0ff25fcb97
Jarren San Jose <sanjose.jarren@gmail.com>
2024-03-17 00:10:22 -0700

    show drawdown chart for now

--- app/drawdown/page.tsx
+++ app/drawdown/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
-// import DdCalculator from '@/components/DdCalculator'
+import DdCalculator from '@/components/DdCalculator'
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex flex-col flex-grow  items-center p-24 text-center">
         <h1 className="text-4xl mb-2">Drawdown Calculator</h1>
-        {/* <DdCalculator /> */}
+        <DdCalculator />
       </main>
       <footer className="text-center p-4">
         made by&nbsp;
---
+++ components/DdCalculator.tsx
@@ -0,0 +1,42 @@
+const DdCalculator = () => {
+
+  const calculateRecovery = (drawdown: number) => {
+    if (drawdown === 0) return 0;
+    return ((1 / (1 - drawdown / 100)) - 1) * 100;
+  };
+
+  const data = [];
+  for (let i = 5; i <= 95; i = i + 5) {
+    data.push({
+      drawdown: i,
+      recovery: calculateRecovery(i),
+    });
+  }
+
+  return (
+    <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="py-3 px-6">
+              Drawdown (%)
+            </th>
+            <th scope="col" className="py-3 px-6">
+              Recovery required (%)
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td className="py-4 px-6">{item.drawdown.toFixed(2)}%</td>
+              <td className="py-4 px-6">{item.recovery.toFixed(2)}%</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DdCalculator;
